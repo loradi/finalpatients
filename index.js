7d9033067b92215c331cff5ad101ab9e78152d90
loradi <42723805+loradi@users.noreply.github.com>
2018-11-23 15:01:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get ('/', function (request, response){
+response.send("HELLO WORLD")
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+console.log("Listen on port :" + port);
+});
